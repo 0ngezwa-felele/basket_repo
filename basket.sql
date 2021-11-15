4807144c057facf75ed8b800af99661d0e333b12
0ngezwa-felele <ongezwa@codex.com>
2021-11-15 15:52:01 +0200

    travis file

--- basket.sql
+++ basket.sql
@@ -4,6 +4,3 @@ create table fruit_basket(
    price decimal(10,2),
    quantity integer not null
 );
--- INSERT INTO fruit_basket(type,price,quantity) VALUES('Orange',4.00,5);
--- INSERT INTO fruit_basket(type,price,quantity) VALUES('Apple',2.00,4);
--- INSERT INTO fruit_basket(type,price,quantity) VALUES('Banana',5.00,5);
